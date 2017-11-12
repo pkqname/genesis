7a27360e89c1c3ca694bf3d5c7a5b248b34f3abe
Администратор <fldenismarchuk@gmail.com>
2017-11-13 01:01:27 +0200

    add migration

--- src/migrations/m170306_083010_notes.php
+++ src/migrations/m170306_083010_notes.php
@@ -2,7 +2,7 @@
 
 use yii\db\Migration;
 
-class m170306_083010_statistics extends Migration
+class m170306_083010_notes extends Migration
 {
 
     // Use safeUp/safeDown to run migration code within a transaction
@@ -15,19 +15,17 @@ class m170306_083010_statistics extends Migration
         }
 
 		//Создание таблицы IP пользователей
-		$this->createTable('{{%ksl_ip_count}}', [
+		$this->createTable('{{%notes}}', [
 			'id' => $this->primaryKey(),
-			'ip' => $this->string(15)->notNull(),
-			'str_url' => $this->string(255),
-			'date_ip' => $this->integer(),
-			'black_list_ip' => $this->boolean()->defaultValue(0)->notNull(),
-			'comment' => $this->string(50),
+			'text' => $this->string(255),
+            'date' => $this->dateTime(),
+            'date_change' => $this->dateTime()
 		], $tableOptions);
 
     }
 
     public function safeDown()
     {
-			$this->dropTable('{{%ksl_ip_count}}');
+			$this->dropTable('{{%notes}}');
     }
 }
