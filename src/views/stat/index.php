205fbae4a7dbd5046b1e7fc07daaed1525ab6f0d
Администратор <fldenismarchuk@gmail.com>
2017-11-13 06:19:18 +0200

    fixed

--- src/views/stat/index.php
+++ src/views/stat/index.php
@@ -6,8 +6,8 @@
         <div class="note">
             <h3>Заметка №<?=$note->id?></h3>
             <p><?=$note->text?></p>
-            <span>Дата создания <?=$note->date?></span>
-            <span style="float:right">Дата последнего изменения <?=$note->date_change?></span><br>
+            <span class="date">Дата создания <?=$note->date?></span>
+            <span class="date" style="float:right">Дата последнего изменения <?=$note->date_change?></span><br><br>
             <a class="btn-default view" href="notes/<?=$note->id?>">Просмотреть</a>
         </div>
     <?php } ?>
--- src/views/stat/single.php
+++ src/views/stat/single.php
@@ -1,7 +1,9 @@
 <div class="container">
     <h3>Заметка №<?=$note->id?></h3>
-    <p><?=$note->text?></p>
-    <p>Дата создания <?=$note->date?></p>
+    <p class="text"><?=$note->text?></p>
+    <p class="date">Дата создания <?=$note->date?></p>
+    <p class="date">Дата создания <?=$note->date_change?></p>
     <a class="edit btn-primary" href="/notes/edit/<?=$note->id?>">Редактировать</a>
-    <a class="delete btn-danger" href="/notes/delete/<?=$note->id?>">Удалить</a>
+    <a class="delete btn-danger" href="/notes/delete/<?=$note->id?>">Удалить</a><br><br><br>
+    <a class="btn-link" href="/notes">На главную</a>
 </div>
