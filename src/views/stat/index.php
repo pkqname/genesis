bea0ec29371812ab0a6ae92de25ea833ed8f0e0b
Администратор <fldenismarchuk@gmail.com>
2017-11-13 06:30:03 +0200

    fixed

--- src/views/stat/index.php
+++ src/views/stat/index.php
@@ -1,15 +1,15 @@
 <div class="container">
     <h1>Список заметок</h1>
-     <a class="added btn-success" href="notes/added">Добавить заметку</a>
+    <a class="added btn-success" href="notes/added">Добавить заметку</a>
     <div class="notes">
-    <?php foreach($notes as $note) { ?>
-        <div class="note">
-            <h3>Заметка №<?=$note->id?></h3>
-            <p><?=$note->text?></p>
-            <span class="date">Дата создания <?=$note->date?></span>
-            <span class="date" style="float:right">Дата последнего изменения <?=$note->date_change?></span><br><br>
-            <a class="btn-default view" href="notes/<?=$note->id?>">Просмотреть</a>
-        </div>
-    <?php } ?>
+        <?php foreach($notes as $note) { ?>
+            <div class="note">
+                <h3>Заметка №<?=$note->id?></h3>
+                <p class="text"><?=$note->text?></p>
+                <span class="date">Дата создания <?=$note->date?></span>
+                <span class="date" style="float:right">Дата последнего изменения <?=$note->date_change?></span><br><br>
+                <a class="btn-primary view" href="notes/<?=$note->id?>">Просмотреть</a>
+            </div>
+        <?php } ?>
     </div>
 </div>
