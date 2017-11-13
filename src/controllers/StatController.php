2f52c5a89ba2e14eaa5c8e1397e362a2b971ee7b
Администратор <fldenismarchuk@gmail.com>
2017-11-13 06:43:42 +0200

    fixed

--- src/controllers/StatController.php
+++ src/controllers/StatController.php
@@ -2,11 +2,9 @@
 
 namespace Pkqname\Notes\controllers;
 
-use Pkqname\Notes\models\Notes;
-use Pkqname\Notes\StatAssetsBundle;
 use Yii;
 use yii\web\Controller;
-
+use Klisl\Statistics\models\Model;
 
 
 
@@ -15,67 +13,13 @@ class StatController extends Controller
 
     public $layout = 'main';
 
-    public function actionIndex()
+    public function actionIndex($condition = [], $days_ago = null, $stat_ip = false)
     {
         // регистрируем ресурсы:
-        StatAssetsBundle::register($this->view);
-
-        $notes = Notes::find()->all();
+        \Pkqname\Notes\StatAssetsBundle::register($this->view);
 
 
-        return $this->render('index', compact('notes'));
-    }
-
-    public function actionSingle(int $id=null)
-    {
-        // регистрируем ресурсы:
-        StatAssetsBundle::register($this->view);
-
-        $note = Notes::find()->where(['id' => $id])->one();
-
-        return $this->render('single', compact('note'));
-    }
-
-    public function actionEdit(int $id=null)
-    {
-        // регистрируем ресурсы:
-        StatAssetsBundle::register($this->view);
-
-        $model = Notes::find()->where(['id' => $id])->one();
-        if ($model->load(Yii::$app->request->post())){
-            $model->date_change = date("Y-m-d H:i:s");
-            $model->save();
-            return $this->redirect(['/notes/'.$model->id]);
-        }
-
-        return $this->render('edit', compact('model'));
-    }
-
-    public function actionDelete(int $id=null)
-    {
-        // регистрируем ресурсы:
-        StatAssetsBundle::register($this->view);
-
-        $model = Notes::find()->where(['id' => $id])->one();
-        $model->delete();
-
-        return $this->redirect(['notes']);
-    }
-
-    public function actionAdded()
-    {
-        // регистрируем ресурсы:
-        StatAssetsBundle::register($this->view);
-
-        $model = new Notes();
-        if($model->load(Yii::$app->request->post())) {
-            $model->date = date("Y-m-d H:i:s");
-            $model->date_change = date("Y-m-d H:i:s");
-            $model->save();
-            return $this->redirect(['/notes/'.$model->id]);
-        }
-
-        return $this->render('edit', compact('model'));
+        return $this->render('index', []);
     }
 
 
